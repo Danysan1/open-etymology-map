185461810309c041b0525d9f1cb7fc6e27f6cb05
Daniele Santini <danysan95@gmail.com>
2023-02-26 11:33:40 +0100

    WIP switch to OSM-Wikidata Map Framework

--- airflow/dags/sql/etymology-foreign-key.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE oem.etymology 
-ADD CONSTRAINT etymology_et_el_id_fkey 
-FOREIGN KEY (et_el_id) 
-REFERENCES oem.element (el_id);
--- airflow/dags/sql/global-map-view.sql
+++
@@ -1,6 +0,0 @@
-CREATE MATERIALIZED VIEW oem.vm_global_map AS
-SELECT
-    ST_ReducePrecision(ST_Centroid(el_geometry), 0.3) AS geom,
-    COUNT(DISTINCT LOWER(el_tags->>'name')) AS num
-FROM oem.element
-GROUP BY geom;
